e73acd4081b6acfc62848d510ab6bea9e3f14ab1
Potato <raph4928@gmail.com>
2021-10-18 22:01:02 -0400

    added models and services folder

---
+++ WeatherApp/Models/WindDataModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.Models
+{
+    public class WindDataModel
+    {
+        public DateTime DateTime;
+        public double MetrePerSec;
+        public double Direction;
+    }
+}
---
+++ WeatherApp/Services/ArduinoWeatherStationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+
+namespace WeatherApp.Services
+{
+    public class ArduinoWeatherStationService : IWinDataService
+    {
+        public async Task<WindDataModel> GetDataAsync()
+        {
+            WindDataModel windDataModel = new WindDataModel();
+            return windDataModel;
+        }
+    }
+}
